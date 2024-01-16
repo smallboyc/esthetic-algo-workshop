4a258dce6102e9bff7b93c5f75b733c6d3272154
Maxence Dupuis <smallboyc@MacBook-Pro-de-Max.local>
2024-01-16 18:55:21 +0100

    feat add comments in exo02

--- exo02/sketch.js
+++ exo02/sketch.js
@@ -2,46 +2,50 @@ function setup() {
   createCanvas(1000, 1000);
   background(255);
   noStroke();
-  frameRate(0.5);
-  // selectPalette();
+  frameRate(1);
+  selectPalette();
 }
 
-// const palette = new Array(12);
+const palette = new Array(12);
 
-const fixedPalette = [
-  "#00120B",
-  "#011B11",
-  "#6B818C",
-  "#31E981",
-  "#05160F",
-  "#E49AB0",
-  "#FFB700",
-  "#66757D",
-  "#439165",
-  "#000000",
-  "#515B60",
-];
+//Palette fixe (peut être remplacée par "palette" à la ligne 46)
+// const fixedPalette = [
+//   "#00120B",
+//   "#011B11",
+//   "#6B818C",
+//   "#31E981",
+//   "#05160F",
+//   "#E49AB0",
+//   "#FFB700",
+//   "#66757D",
+//   "#439165",
+//   "#000000",
+//   "#515B60",
+// ];
 
+//Fonction qui remplie un tableau (palette) avec 12 couleurs aléatoires.
 function selectPalette() {
   const range = 255;
   for (let i = 0; i < 12; i++) {
-    let r = random(0, range);
-    let v = random(0, range);
-    let b = random(0, range);
+    //on peut ajuster l'intervalle pour cibler l'importance de certaines composantes.
+    let r = random(30, range - 10);
+    let v = random(78, range);
+    let b = random(89, range - 23);
     let a = random(0, range);
     palette[i] = color(r, v, b, a);
   }
 }
 
+//Fonction qui gènère une grille de 10x10 cases, centrée sur le canvas.
 function newGrid() {
   let n = 50;
   const scale = 10;
-  const gap = (width - n * scale) / 2;
+  const margin = (width - n * scale) / 2;
   for (let i = 0; i < scale; i++)
     for (let j = 0; j < scale; j++) {
-      let color = random(fixedPalette);
+      let color = random(palette); //remplacez "palette" par "fixedPalette" pour des couleurs non aléatoires.
       fill(color);
-      square(gap + n * i, gap + n * j, n);
+      square(margin + n * i, margin + n * j, n);
     }
 }
 
